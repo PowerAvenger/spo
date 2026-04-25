91a72c746c852bb6fe5186447d871813a58abc6a
Jose <>
2026-04-25 07:59:44 +0200

    abierto may26

--- pages/main.py
+++ pages/main.py
@@ -479,8 +479,8 @@ with tab5:
 
 # APUESTAS --------------------------------------------------------------------------------------------------------------------------------
 #provisional
-mes_apuesta = None
-#mes_apuesta = 'may-26'
+#mes_apuesta = None
+mes_apuesta = 'may-26'
 
 
 with tab6:
