5575c7cb72926c6d87bd64a252a5fda6b3661807
Jose <>
2026-09-25 08:09:03 +0200

    abierto registro oct26

--- pages/main.py
+++ pages/main.py
@@ -587,8 +587,8 @@ with tab5:
 
 # APUESTAS --------------------------------------------------------------------------------------------------------------------------------
 #provisional
-mes_apuesta = None
-#mes_apuesta = 'oct-26'
+#mes_apuesta = None
+mes_apuesta = 'oct-26'
 
 
 with tab6:
